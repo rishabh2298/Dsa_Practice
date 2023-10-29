1f68f2ad3a23a82a58a36348cd9f93f9b6acb946
rishabh2298 <rishabhmishra2298@gmail.com>
2023-10-30 00:57:14 +0530

    InOrder Traversal Tree Tc=O(N)

---
+++ Trees/InOrderTraversal.java
@@ -0,0 +1,62 @@
+package Dsa_Practice.Trees;
+
+public class InOrderTraversal {
+	
+	static class Node{
+		int data;
+		Node leftNode;
+		Node rightNode;
+		
+		public Node(int data) {
+			this.data = data;
+			this.leftNode = null;
+			this.rightNode = null;
+		}
+	}
+	
+	
+	static class BinaryTree{
+		
+		static int idx = -1;
+		
+		public static Node builtTree(int[] nodes) {
+			idx++;
+			
+			if(nodes[idx] == -1) {
+				return null;
+			}
+			
+			Node newNode = new Node(nodes[idx]);
+			newNode.leftNode = builtTree(nodes);
+			newNode.rightNode = builtTree(nodes);
+			
+			return newNode;
+		}
+		
+	}
+	
+	
+	public static void inOrder(Node node) {
+		
+		if(node == null) {
+			return;
+		}
+		
+		
+		inOrder(node.leftNode);
+		System.out.print(node.data + " ");
+		inOrder(node.rightNode);
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		int[] nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+		
+		Node rootNode = BinaryTree.builtTree(nodes);
+		
+		inOrder(rootNode);
+	}
+	
+}
