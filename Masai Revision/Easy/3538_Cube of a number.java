52a57481c934acb59948f0e5f9b60fd42989a69b
rishabh2298 <rishabhmishra2298@gmail.com>
2024-01-26 23:41:58 +0530

    Easy-05

---
+++ Masai Revision/Easy/3538_Cube of a number.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+            int value = scanner.nextInt();
+            System.out.println(Math.round(Math.pow(value,3)));
+    }
+}
