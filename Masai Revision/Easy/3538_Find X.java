374e8d4dbdda6176d94813811f283da7df3be9cf
rishabh2298 <rishabhmishra2298@gmail.com>
2024-01-26 23:33:11 +0530

    Easy_07

---
+++ Masai Revision/Easy/3538_Find X.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        
+        int N = scanner.nextInt();
+        
+        System.out.println(N*3+10);
+    }
+}
