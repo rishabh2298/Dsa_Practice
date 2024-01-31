3681969888f910c1c859cd4e2c1755ea31906b59
rishabh2298 <rishabhmishra2298@gmail.com>
2024-01-31 14:45:29 +0530

    Medium-14, Tc=O(N) Sc=O(N), Array

--- Masai Revision/Medium/4097_Rotate the array returns back.java
+++ Masai Revision/Medium/4097_Rotate the array returns back.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Main{
     
-    private static void rotateElements(int N, long K, int[] arr){
+    private static void rotateElements1(int N, long K, int[] arr){
         
         int k;
         if(K > N){
@@ -28,6 +28,42 @@ class Main{
         System.out.println(ans.toString());
     }
     
+    
+    private static void rotateElements2(int N, long K, int[] arr){
+        
+        int k;
+        
+        if(K > N){
+            k = (int)(K%N);
+        }
+        else{
+            k = (int)K;
+        }
+        
+        
+        int[] result = new int[N];
+        int j=0;    // for result index
+        
+        // save 1st half of rotated array
+        // so start from N-k because that will become 1st element
+        
+        for(int i=N-k; i<N; i++){
+            result[j++] = arr[i];
+        }
+            
+        // till N-k, cause that will last element, try dry run
+        
+        for(int i=0; i<N-k && j<N; i++)
+            result[j++] = arr[i];
+            
+        for(int i=0; i<N; i++)
+            System.out.print(result[i]+" ");
+            
+        // to print next line
+        System.out.println();
+    }
+    
+    
     public static void main(String[] args){
         
         Scanner scanner = new Scanner(System.in);
@@ -41,7 +77,12 @@ class Main{
             scanner.nextLine();
             int[] arr = Arrays.stream(scanner.nextLine().trim().split(" ")).mapToInt(Integer::parseInt).toArray();
             
-            rotateElements(N,K,arr);
+            // Tc=O(N * (K%N)) Sc=O(1);
+            rotateElements1(N,K,arr);
+            
+            // Tc=O(N) Sc=O(N);
+            rotateElements2(N,K,arr);
+            
         }
     }
 }
