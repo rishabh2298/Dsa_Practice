2897c4f8657d3a84c590c89cafeea928e563d065
rishabh2298 <rishabhmishra2298@gmail.com>
2024-08-14 00:41:05 +0530

    heightOfTree Tc=O(N), Sc=O(H)

---
+++ Trees/Tree(heightOfTree, maxNode).java
@@ -0,0 +1,59 @@
+/*
+
+Height of Tree = 4
+
+*/
+
+class HelloWorld {
+    
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        
+        public Node(){
+            this.data = 0;
+            this.left = null;
+            this.right = null;
+        }
+        
+        public Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+        int[] arr = {1,2,4,7,-1,-1,8,-1,-1,5,-1,9,-1,-1,3,6,-1,10,-1,-1,-1};
+        
+        Node node = buildTree(arr);
+        
+        int height = heightOfTree(node);
+        System.out.println("Height of Tree = " + height);
+        
+    }
+    
+    static int index = -1;
+    private static Node buildTree(int[] arr){
+        index++;
+        if(arr[index] == -1){
+            return null;
+        }
+        
+        Node node = new Node(arr[index]);
+        node.left = buildTree(arr);
+        node.right = buildTree(arr);
+        
+        return node;
+    }
+    
+    private static int heightOfTree(Node node){
+        if(node == null){
+            return 0;
+        }
+        
+        return 1 + Math.max(heightOfTree(node.left), heightOfTree(node.right));
+    }
+}
